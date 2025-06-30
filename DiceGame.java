45ac591d9527b745bf8e460c5477e36872184d3a
Richard-M-M <morimuromail@gmail.com>
2025-06-30 17:08:41 +0900

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -1,10 +1,16 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class DiceGame {
     public static void main(String[] args) {
         Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
         int die1 = rand.nextInt(6) + 1;
         int die2 = rand.nextInt(6) + 1;
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
 
         System.out.println("Rolling dice...");
         System.out.println("Die 1: " + die1);
